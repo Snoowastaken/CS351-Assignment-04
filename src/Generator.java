71ee76a7fd1e01fe333352c42f7fe5e1a7d2559c
snoo <wm28744@gmail.com>
2023-04-03 15:32:18 -0500

    epic

---
+++ src/Generator.java
@@ -0,0 +1,14 @@
+import desmoj.core.simulator.*;
+import java.util.concurrent.TimeUnit;
+import co.paralleluniverse.fibers.SuspendExecution;
+
+public class Generator extends SimProcess {
+
+    public Generator(Model owner, String name, boolean showInTrace) {
+        super(owner, name, showInTrace);
+    }
+
+    public void lifeCycle() throws SuspendExecution {
+        MachineShop model = (MachineShop)getModel();
+    }
+}
