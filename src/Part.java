c244c46d31c92be54ab7cb2afc561d29e396ef32
snoo <wm28744@gmail.com>
2023-04-09 02:26:10 -0500

    kjkj

--- src/Part.java
+++ src/Part.java
@@ -1,5 +1,6 @@
 import co.paralleluniverse.fibers.SuspendExecution;
 import desmoj.core.simulator.*;
+import java.util.concurrent.TimeUnit;
 
 public class Part extends SimProcess {
 
@@ -9,6 +10,46 @@ public class Part extends SimProcess {
 
     public void lifeCycle() throws SuspendExecution {
         MachineShop model = (MachineShop)getModel();
+        boolean needsRefining = false;
+        model.numberOfParts.update();
+        model.processingQueue.insert(this);
+        if(model.machineIsBusy){
+            passivate();
+        } else {
+            model.machineIsBusy = true;
+            model.processingQueue.removeFirst();
+            if(!needsRefining){
+                //sample processing time
+                double processingTime = model.processingTime.sample();
+                hold(new TimeSpan(processingTime, TimeUnit.MINUTES));
+                model.machineIsBusy = false;
+                model.inspectionQueue.insert(this);
+                if(model.inspectorIsBusy){
+                    passivate();
+                } else {
+                    model.inspectorIsBusy = true;
+                    model.inspectionQueue.removeFirst();
+                    //sample inspection time
+                    double inspectionTime = model.inspectionTime.sample();
+                    hold(new TimeSpan(inspectionTime, TimeUnit.MINUTES));
+                    needsRefining = model.needsRefining.sample();
+                    model.inspectorIsBusy = false;
+                    if(needsRefining){
+
+                    }
+                }
+
+            }
+
+
+
+        }
+
+
+
+
+
+
 
 
 
